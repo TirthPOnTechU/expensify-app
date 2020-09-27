cd7446c3258bf06749fec563351017ab52438db6
TirthPOnTechU <tirth.patel1@ontariotechu.net>
2020-09-27 19:09:19 -0400

    changed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import{connect} from "react-redux"
 import {startLogout} from "../actions/firebase"
 const Header=(props)=>(
     <header>
-      <h1>Header</h1>
+      <h1>EXPENSIFY</h1>
       <NavLink exact={true} activeClassName='is-active' to='/dashboard'>Dashboard</NavLink>
       <NavLink activeClassName='is-active' to='/create'>Create</NavLink>
       <button onClick={()=>props.dispatch(startLogout())}>Sign Out</button>
